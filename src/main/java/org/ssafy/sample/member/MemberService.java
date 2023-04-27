1822221e818a64ee209e9055f3062f6ec42decf2
ygangmin <ygangmin@gmail.com>
2023-04-27 17:43:37 +0900

    트랜잭션, 테스트

--- src/main/java/org/ssafy/sample/member/MemberService.java
+++ src/main/java/org/ssafy/sample/member/MemberService.java
@@ -12,16 +12,13 @@ public class MemberService {
         this.memberDao = memberDao;
     }
 
-    @Transactional
+    @Transactional(rollbackFor = {Exception.class})
     public void createMember(MemberDto memberDto){
-        memberDao.createMember(memberDto);
-        String s = null;
-        s.length();
-        memberDao.createMember(memberDto);
+        memberDao.saveMemberDto(memberDto);
     }
 
     public MemberDto isValidMember(MemberDto memberDto) throws NotValidUserException {
-        MemberDto findMember = memberDao.findMember(memberDto.getId());
+        MemberDto findMember = memberDao.findMemberById(memberDto.getId());
         if(findMember == null) throw new NullPointerException("없는 아이디");
         if(memberDto.getPassword().equals(findMember.getPassword())){
             return findMember;
--- src/test/java/org/ssafy/sample/SpringSampleApplicationTests.java
+++ src/test/java/org/ssafy/sample/SpringSampleApplicationTests.java
@@ -1,13 +1,30 @@
 package org.ssafy.sample;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.ssafy.sample.member.MemberService;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ContextConfiguration(classes = SpringSampleApplication.class)
 class SpringSampleApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private MemberService memberService;
+	@BeforeAll
+	void beforeTest(){
+
 	}
+
+
+	@Test
+	void testService(){
+		assertNotNull(memberService);
+	}
+
 
 }
